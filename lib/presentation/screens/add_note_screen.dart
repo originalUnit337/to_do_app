8dbe522d023c633702e2d332da1fb2a7a6d01c33
Unit337 <devguyvf@gmail.com>
2025-05-02 18:13:23 +0300

    Added full translation to add_note_screen (eng, rus)

--- lib/presentation/screens/add_note_screen.dart
+++ lib/presentation/screens/add_note_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:to_do_app/navigation/app_router.dart';
+import 'package:to_do_app/presentation/theme/app_theme.dart';
+import 'package:to_do_app/presentation/ui_kit/font/app_font_style.dart';
 
 class AddNoteScreen extends StatelessWidget {
   const AddNoteScreen({super.key});
@@ -17,6 +19,21 @@ class AddNoteScreen extends StatelessWidget {
           TextButton(
             onPressed: () {},
             child: Text(AppLocalizations.of(context)!.save),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: AppLocalizations.of(context)!.what_should_i_do,
+                hintStyle: AppFontStyle.title,
+              ),
+              minLines: 4,
+              maxLines: 6,
+            ),
           ),
         ],
       ),
