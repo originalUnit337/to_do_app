8fffb5626b47d198a1c83e12c9e78fd267801064
Unit337 <devguyvf@gmail.com>
2025-05-02 20:40:33 +0300

    AddNoteScreen in progress.

--- lib/presentation/screens/add_note_screen.dart
+++ lib/presentation/screens/add_note_screen.dart
@@ -9,7 +9,13 @@ class AddNoteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _dropdownMenuEntries = <String>[
+      AppLocalizations.of(context)!.no,
+      AppLocalizations.of(context)!.low,
+      AppLocalizations.of(context)!.high,
+    ];
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
         leading: IconButton(
           onPressed: AppRouter.router.pop,
@@ -22,20 +28,48 @@ class AddNoteScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
               decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(8),
+                ),
                 hintText: AppLocalizations.of(context)!.what_should_i_do,
                 hintStyle: AppFontStyle.title,
+                fillColor: Colors.white,
+                filled: true,
               ),
               minLines: 4,
               maxLines: 6,
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            Text(
+              AppLocalizations.of(context)!.importance,
+              style: AppFontStyle.title,
+            ),
+            DropdownMenu(
+              menuStyle: MenuStyle(),
+              initialSelection: _dropdownMenuEntries.first,
+              dropdownMenuEntries:
+                  _dropdownMenuEntries.map((e) {
+                    return DropdownMenuEntry(value: e, label: e);
+                  }).toList(),
+            ),
+            Divider(),
+            // TODO: switch + data picker
+            Divider(),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(AppLocalizations.of(context)!.delete),
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
       ),
     );
   }
