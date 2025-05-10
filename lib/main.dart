0449dd7889b8fbf108b3b3095145fed4dad17277
Bobr PC <devguyvf@gmail.com>
2025-05-10 23:41:33 +0300

    Added palette provider, better app theme handle

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'package:to_do_app/localization/l10n/l10n.dart';
 import 'package:to_do_app/navigation/app_router.dart';
 import 'package:to_do_app/presentation/screens/home_screen.dart';
 import 'package:to_do_app/presentation/theme/app_theme.dart';
+import 'package:to_do_app/presentation/ui_kit/palette/dark.dart';
+import 'package:to_do_app/presentation/ui_kit/palette/light.dart';
+import 'package:to_do_app/presentation/ui_kit/palette/palette_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,14 +21,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      supportedLocales: L10n.all,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      theme: AppTheme.lightAppTheme,
-      darkTheme: AppTheme.darkAppTheme,
-      routerConfig: AppRouter.router,
-      // routeInformationParser: AppRouter.router.routeInformationParser,
-      // routerDelegate: AppRouter.router.routerDelegate,
+    final currentPalette = MediaQuery.of(context).platformBrightness == Brightness.light ? LightPalette() : DarkPalette();
+    return PaletteProvider(
+      palette: currentPalette,
+      child: MaterialApp.router(
+        supportedLocales: L10n.all,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        theme: AppTheme.getAppTheme(currentPalette),
+        //darkTheme: AppTheme.darkAppTheme,
+        routerConfig: AppRouter.router,
+        // routeInformationParser: AppRouter.router.routeInformationParser,
+        // routerDelegate: AppRouter.router.routerDelegate,
+      ),
     );
   }
 }
