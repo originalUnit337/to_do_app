4a8916182ef3ecacd7bb0da2147e44c50bb6e7c5
Unit337 <devguyvf@gmail.com>
2025-06-01 20:52:57 +0300

    Implemented note_repository

--- lib/data/api/repository/note_repository_impl.dart
+++ lib/data/api/repository/note_repository_impl.dart
@@ -1,11 +1,34 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:to_do_app/core/constants/constants.dart';
 import 'package:to_do_app/core/resources/data_state.dart';
 import 'package:to_do_app/data/api/model/note_model.dart';
+import 'package:to_do_app/data/data_sources/remote/note_api_service.dart';
 import 'package:to_do_app/domain/repository/note_repository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
+  final NoteApiService _noteApiService;
+  NoteRepositoryImpl(this._noteApiService);
   @override
-  Future<DataState<List<NoteModel>>> getAllNotes() {
-    // TODO: implement getAllNotes
-    throw UnimplementedError();
+  Future<DataState<List<NoteModel>>> getAllNotes() async {
+    try {
+      final httpResponse = await _noteApiService.getAllNotes(apiKey: apiKey);
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
