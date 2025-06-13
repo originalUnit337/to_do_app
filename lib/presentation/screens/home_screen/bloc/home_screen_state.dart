0b68cb9ca9985c02bb526c98659c2615d3379740
Unit337 <devguyvf@gmail.com>
2025-06-13 22:00:46 +0300

    Fixed snackbar notification.

--- lib/presentation/screens/home_screen/bloc/home_screen_state.dart
+++ lib/presentation/screens/home_screen/bloc/home_screen_state.dart
@@ -1,28 +1,39 @@
 import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
 import 'package:to_do_app/domain/entities/note.dart';
 
-sealed class HomeScreenState {
+sealed class HomeScreenState extends Equatable {
   final List<NoteEntity>? notes;
   final bool showCompleted;
   final DioException? exception;
 
-  HomeScreenState({this.notes, this.exception, this.showCompleted = true});
+  const HomeScreenState({
+    this.notes,
+    this.exception,
+    this.showCompleted = true,
+  });
+
+  @override
+  List<Object?> get props => [notes, showCompleted, exception];
 }
 
 class NotesInitial extends HomeScreenState {
-  NotesInitial();
+  const NotesInitial();
 }
 
 class NotesLoading extends HomeScreenState {
-  NotesLoading();
+  const NotesLoading();
 }
 
 class NotesLoaded extends HomeScreenState {
-  String? message;
-  NotesLoaded(List<NoteEntity> notes, {super.showCompleted, this.message})
+  final String? message;
+  const NotesLoaded(List<NoteEntity> notes, {super.showCompleted, this.message})
     : super(notes: notes);
+
+  @override
+  List<Object?> get props => [notes, showCompleted, exception, message];
 }
 
 class NotesError extends HomeScreenState {
-  NotesError(DioException exception) : super(exception: exception);
+  const NotesError(DioException exception) : super(exception: exception);
 }
--- lib/presentation/screens/home_screen/widgets/build_notes_list.dart
+++ lib/presentation/screens/home_screen/widgets/build_notes_list.dart
@@ -26,193 +26,171 @@ class BuildNotesList extends StatelessWidget {
     final currentPalette = AppPalette.of(context);
     //bool showCompleted = true;
 
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
-      slivers: [
-        SliverAppBar(
-          pinned: true,
-          actions: [
-            IconButton(
-              icon: Icon(
-                showCompleted ? Icons.visibility_off : Icons.visibility,
-                color: currentPalette.colorBlue,
+    return BlocListener<HomeScreenBloc, HomeScreenState>(
+      listener: (context, state) {
+        if (state is NotesLoaded) {
+          if (state.message != null) {
+            final snackBar = SnackBar(
+              //TODO Add localization
+              content: Text(state.message ?? 'Something went wrong'),
+              duration: const Duration(seconds: 2),
+              action: SnackBarAction(label: 'Close', onPressed: () {}),
+            );
+
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+        }
+      },
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            actions: [
+              IconButton(
+                icon: Icon(
+                  showCompleted ? Icons.visibility_off : Icons.visibility,
+                  color: currentPalette.colorBlue,
+                ),
+                onPressed: () {
+                  BlocProvider.of<HomeScreenBloc>(
+                    context,
+                  ).add(ToggleShowCompleted());
+                },
               ),
-              onPressed: () {
-                BlocProvider.of<HomeScreenBloc>(
-                  context,
-                ).add(ToggleShowCompleted());
-              },
-            ),
-          ],
-          expandedHeight: 140,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text(
-              AppLocalizations.of(context)?.myNotes ?? 'My notes',
-              style: AppFontStyle.largeTitle,
+            ],
+            expandedHeight: 140,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                AppLocalizations.of(context)?.myNotes ?? 'My notes',
+                style: AppFontStyle.largeTitle,
+              ),
             ),
           ),
-        ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 24,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 24, left: 60),
-              child: Text(
-                '${AppLocalizations.of(context)?.completed ?? 'Completed'} — ${noteItems.where((e) {
-                  return e.isCompleted;
-                }).length}',
-                style: AppFontStyle.body.copyWith(
-                  color: currentPalette.labelTertiary,
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 24,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 24, left: 60),
+                child: Text(
+                  '${AppLocalizations.of(context)?.completed ?? 'Completed'} — ${noteItems.where((e) {
+                    return e.isCompleted;
+                  }).length}',
+                  style: AppFontStyle.body.copyWith(
+                    color: currentPalette.labelTertiary,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        const SliverToBoxAdapter(child: SizedBox(height: 16)),
-        SliverPadding(
-          padding: const EdgeInsets.only(left: 16, right: 16),
-          sliver: DecoratedSliver(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 2,
-                  offset: const Offset(0, 2),
-                  color: Colors.black.withAlpha(12),
-                ),
-                BoxShadow(color: Colors.black.withAlpha(6), blurRadius: 2),
-              ],
-              color: currentPalette.backSecondary,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                childCount: noteItems.length,
-                (context, index) {
-                  if (!showCompleted && noteItems[index].isCompleted) {
-                    return const SizedBox.shrink();
-                  }
-                  return Dismissible(
-                    key: UniqueKey(),
-                    direction:
-                        showCompleted
-                            ? DismissDirection.endToStart
-                            : DismissDirection.horizontal,
-                    background: Container(
-                      color: Colors.green,
-                      alignment: Alignment.centerLeft,
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 24),
-                        child: Icon(Icons.check, color: Colors.white),
-                      ),
-                    ),
-                    onDismissed: (direction) {
-                      if (direction == DismissDirection.startToEnd) {
-                        BlocProvider.of<HomeScreenBloc>(
-                          context,
-                        ).add(ToggleNoteCompletion(noteItems[index]));
-                        BlocListener<HomeScreenBloc, HomeScreenState>(
-                          listener: (context, state) {
-                            if (state is NotesLoaded) {
-                              if (state.message != null) {
-                                final snackBar = SnackBar(
-                                  //TODO Add localization
-                                  content: Text(
-                                    message ?? 'Something went wrong',
-                                  ),
-                                  duration: const Duration(seconds: 2),
-                                  action: SnackBarAction(
-                                    label: 'Close',
-                                    onPressed: () {},
-                                  ),
-                                );
-
-                                ScaffoldMessenger.of(
-                                  context,
-                                ).showSnackBar(snackBar);
-                              }
-                            }
-                          },
-                        );
-
-                        //noteItems[index].isCompleted = true;
-                      } else {}
-                    },
-                    secondaryBackground: Container(
-                      color: Colors.red,
-                      alignment: Alignment.centerRight,
-                      child: const Padding(
-                        padding: EdgeInsets.only(right: 24),
-                        child: Icon(Icons.delete, color: Colors.white),
-                      ),
-                    ),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 4,
-                        horizontal: 4,
-                      ),
-                      title: Row(
-                        children: [
-                          if (noteItems[index].importance == '2')
-                            Text(
-                              '!! ',
-                              style: TextStyle(
-                                color: currentPalette.colorRed,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          if (noteItems[index].importance == '1')
-                            Text(
-                              '↓ ',
-                              style: TextStyle(
-                                color: currentPalette.colorGray,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          Expanded(
-                            child: Text(
-                              noteItems[index].textNote ?? 'Empty',
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                              style:
-                                  noteItems[index].isCompleted
-                                      ? AppFontStyle.body.copyWith(
-                                        color: currentPalette.labelTertiary,
-                                        decoration: TextDecoration.lineThrough,
-                                        decorationColor:
-                                            currentPalette.labelTertiary,
-                                      )
-                                      : AppFontStyle.body,
-                            ),
-                          ),
-                        ],
-                      ),
-                      leading: Checkbox(
-                        value: noteItems[index].isCompleted ? true : false,
-                        onChanged: (value) {},
-                        side: BorderSide(
-                          color:
-                              noteItems[index].importance == '2'
-                                  ? currentPalette.colorRed
-                                  : currentPalette.supportSeparator,
-                          width: 2,
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            sliver: DecoratedSliver(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 2,
+                    offset: const Offset(0, 2),
+                    color: Colors.black.withAlpha(12),
+                  ),
+                  BoxShadow(color: Colors.black.withAlpha(6), blurRadius: 2),
+                ],
+                color: currentPalette.backSecondary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: noteItems.length,
+                  (context, index) {
+                    if (!showCompleted && noteItems[index].isCompleted) {
+                      return const SizedBox.shrink();
+                    }
+                    return Dismissible(
+                      key: UniqueKey(),
+                      direction:
+                          showCompleted
+                              ? DismissDirection.endToStart
+                              : DismissDirection.horizontal,
+                      background: Container(
+                        color: Colors.green,
+                        alignment: Alignment.centerLeft,
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Icon(Icons.check, color: Colors.white),
                         ),
                       ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (context) =>
-                                    InfoNoteScreen(note: noteItems[index]),
-                          ),
-                        );
+                      onDismissed: (direction) {
+                        if (direction == DismissDirection.startToEnd) {
+                          BlocProvider.of<HomeScreenBloc>(
+                            context,
+                          ).add(ToggleNoteCompletion(noteItems[index]));
+
+                          //noteItems[index].isCompleted = true;
+                        } else {}
                       },
-                      trailing: IconButton(
-                        icon: Icon(
-                          Icons.info_outline,
-                          color: currentPalette.labelTertiary,
+                      secondaryBackground: Container(
+                        color: Colors.red,
+                        alignment: Alignment.centerRight,
+                        child: const Padding(
+                          padding: EdgeInsets.only(right: 24),
+                          child: Icon(Icons.delete, color: Colors.white),
                         ),
-                        onPressed: () {
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 4,
+                          horizontal: 4,
+                        ),
+                        title: Row(
+                          children: [
+                            if (noteItems[index].importance == '2')
+                              Text(
+                                '!! ',
+                                style: TextStyle(
+                                  color: currentPalette.colorRed,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            if (noteItems[index].importance == '1')
+                              Text(
+                                '↓ ',
+                                style: TextStyle(
+                                  color: currentPalette.colorGray,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            Expanded(
+                              child: Text(
+                                noteItems[index].textNote ?? 'Empty',
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                style:
+                                    noteItems[index].isCompleted
+                                        ? AppFontStyle.body.copyWith(
+                                          color: currentPalette.labelTertiary,
+                                          decoration:
+                                              TextDecoration.lineThrough,
+                                          decorationColor:
+                                              currentPalette.labelTertiary,
+                                        )
+                                        : AppFontStyle.body,
+                              ),
+                            ),
+                          ],
+                        ),
+                        leading: Checkbox(
+                          value: noteItems[index].isCompleted ? true : false,
+                          onChanged: (value) {},
+                          side: BorderSide(
+                            color:
+                                noteItems[index].importance == '2'
+                                    ? currentPalette.colorRed
+                                    : currentPalette.supportSeparator,
+                            width: 2,
+                          ),
+                        ),
+                        onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -222,15 +200,31 @@ class BuildNotesList extends StatelessWidget {
                             ),
                           );
                         },
+                        trailing: IconButton(
+                          icon: Icon(
+                            Icons.info_outline,
+                            color: currentPalette.labelTertiary,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) =>
+                                        InfoNoteScreen(note: noteItems[index]),
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
