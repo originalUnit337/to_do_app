f9674ab3cdc65b28d31adbfcf0132ca525192fbf
Unit337 <devguyvf@gmail.com>
2025-07-08 19:41:02 +0300

    removed app_links package

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,3 @@
-import 'package:app_links/app_links.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:to_do_app/data/api/repository/note_repository_impl.dart';
@@ -12,8 +11,7 @@ import 'package:to_do_app/domain/usecases/update_note.dart';
 final getIt = GetIt.instance;
 
 void initializeDependencies() {
-  getIt..registerSingleton<Dio>(Dio())
-  ..registerSingleton<AppLinks>(AppLinks());
+  getIt.registerSingleton<Dio>(Dio());
   _initServices();
   _initRepositories();
   _initUseCases();
