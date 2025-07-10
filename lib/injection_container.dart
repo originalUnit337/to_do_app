cdea56a1f635d3ef163546c34c2c10c41d59f57d
Unit337 <devguyvf@gmail.com>
2025-07-10 18:45:28 +0300

    Added some logs.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_settings.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:to_do_app/data/api/repository/note_repository_impl.dart';
 import 'package:to_do_app/data/data_sources/local/note_local_service.dart';
 import 'package:to_do_app/data/data_sources/remote/note_api_service.dart';
@@ -13,7 +17,21 @@ import 'package:to_do_app/domain/usecases/update_note.dart';
 final getIt = GetIt.instance;
 
 void initializeDependencies() {
-  getIt.registerSingleton<Dio>(Dio());
+  getIt.registerSingleton<Dio>(
+    Dio()
+      ..interceptors.add(
+        TalkerDioLogger(
+          settings: const TalkerDioLoggerSettings(printResponseData: false),
+        ),
+      ),
+  );
+  Bloc.observer = TalkerBlocObserver(
+    settings: const TalkerBlocLoggerSettings(
+      printChanges: true,
+      printClosings: true,
+      printCreations: true,
+    ),
+  );
   _initServices();
   _initRepositories();
   _initUseCases();
